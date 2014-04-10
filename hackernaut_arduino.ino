b7799c98661af2861cf9913bb46b54b334ee1046
Alex Choi <alex.choi2734@gmail.com>
2014-04-10 01:08:06 -0500

    added .ino file

---
+++ hackernaut_arduino.ino
@@ -0,0 +1,35 @@
+int sensor1 = 4;    // analog in 4
+int sensor2 = 5;    // analog in 5
+                                                
+int LED1 = 3;    // digital 3
+int LED2 = 5;    // digital 5
+                       
+int readVal1;    // voltage value read from analogRead(sensor1)
+int readVal2;    // voltage value read from analogRead(sensor2)
+
+int light = 100;    // set light threshold
+
+void setup(){
+  Serial.begin(9600);    //  setup serial
+  pinMode(LED1, OUTPUT);    //sets up digital 3 as output pin
+  pinMode(LED2, OUTPUT);    //sets up digital 5 as output pin
+}
+
+void loop(){
+  readVal1 = analogRead(sensor1);    // read voltage from sensor1
+  readVal2 = analogRead(sensor2);    // read voltage from sensor2
+  
+  //Serial.println(readVal1);
+  
+  if(readVal1 >= light){    // check if light
+    digitalWrite(LED1, LOW);    // if light, turn off led
+  }else{                      
+    digitalWrite(LED1, HIGH);    // if dark, turn on led
+  }
+  
+  if(readVal2 >= light){    // check if light
+    digitalWrite(LED2, LOW);    // if light, turn off led
+  }else{                      
+    digitalWrite(LED2, HIGH);    // if dark, turn on led
+  }
+}
